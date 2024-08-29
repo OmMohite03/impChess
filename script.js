ce51ea405e2fb3571b491b5967e31bfecd138193
theSporz <omohite41@gmail.com>
2024-08-29 11:51:35 +0530

    rook's moveslist(1/5)
    doesn't work, fix it

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function createLeftTray() {
   return tray;
 }
 
-
+//  actually stores moves for every piece
 let currentPawnMoves = []; 
 
 function colorBox(pawnMoves) {
@@ -121,6 +121,7 @@ function placePiece(event) {
       let currCoordStr = [...currentLoc];
       let [i, j] = currCoordStr;
 
+      // for pawn
       if (selectedPiece == "pawn") {
         let coordJ = parseInt(j) + 1;
         let pawnMoves = i + coordJ;
@@ -131,8 +132,44 @@ function placePiece(event) {
         } else {
           console.log(`possible moves of ${selectedPiece}: movement not possible`);
         }
-      }
-      // Add other piece moves here if needed
+        
+      //   if(i===labels.cols[0]){
+      //     if(j===labels.rows[7]){
+      //       console.log(`possible moves of ${selectedPiece}: ${i}7`);
+      //       colorBox(pawnMoves);
+      //       currentPawnMoves.push(pawnMoves); 
+      //     }
+      //   }
+
+        // filhal ke lie neeche walo se mujhe lena dena nahi
+        // if (i!=labels.cols[0] || j < 8) {
+        //   console.log(`possible moves of ${selectedPiece}: ${i}${coordJ}`);
+        //   colorBox(pawnMoves);
+        //   currentPawnMoves.push(pawnMoves); 
+        // } else {
+        //   console.log(`possible moves of ${selectedPiece}: movement not possible`);
+        // }
+
+      } else if(selectedPiece == "rook"){
+          // looping thorugh cols
+          for(let c = 0; c<labels.cols[8]; c++){             
+            if(i==labels.cols[1]){  //in col A
+              // looping thru rows
+              for(let r=0; r<labels.rows[8]; r++){
+                if(j==labels.rows[8]){ // for row 8, i.e A8
+                  rookMoves="";
+                  for(let c1=0; c1<labels.cols[8]; c1++){ // A1, B1, H1
+                  rookMoves=`${labels.cols[c1], 8}`;   
+                  currentPawnMoves.push(rookMoves);
+                  }
+                }
+              }
+              console.log(`possible moves of ${selectedPiece}: ${currentPawnMoves}`);
+              colorBox(currentPawnMoves);
+            } 
+          }
+        }
+      
     }
 
     possibleMoves(selectedPiece);
